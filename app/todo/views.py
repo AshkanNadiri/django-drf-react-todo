e53a9c3566607638f45abc9e709aa0be0c724652
geekwise-ashkan-nadiri <geekwise.ashkan.nadiri@gmail.com>
2019-11-27 19:58:18 +0000

    finished with views

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .serializer import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
 
-# Create your views here.
+class UserViewSet(viewsets.ModeViewSet):
+    """
+    API endpoint will let you edit or see the users.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModeViewSet):
+    """
+    API endpoint will let you edit or see the groups
+    """
+    queryset = Group.objects.all() 
+    serializer_class = GroupSerializer
