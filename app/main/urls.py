b8a09341110b650e37df4142caff65f2f760448c
geekwise-ashkan-nadiri <geekwise.ashkan.nadiri@gmail.com>
2019-12-04 19:05:10 +0000

    Added: added the  key to the url

--- app/main/urls.py
+++ app/main/urls.py
@@ -10,7 +10,7 @@ router.register(r'groups', todo_views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
